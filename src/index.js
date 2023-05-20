178cf34f8fc85efbe6931e40f95bffb69d607d30
murat72 <muratkunanbaev1991@gmail.com>
2023-05-20 23:32:47 +0500

    modifed RandomChar and optimized  MarvelService files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
-import './style/style.scss';
 
-const marvelService = new MarvelService();
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
+import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
